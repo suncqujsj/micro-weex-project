9719117fcfef905cff7c05745d0faff68380709f
CN\CHENJQ20 <jianqiang1.chen@midea.com>
2018-08-16 16:55:06 +0800

    更新代码

--- src/customer-service/settings/config.js
+++ src/customer-service/settings/config.js
@@ -2,7 +2,7 @@ const appConfig = {
     plugin_version: "0.0.32_20180815",
     plugin_name: '',
     default_locale: 'cn',
-    enable_debug: false,
+    enable_debug: !false,
     isMock: false
 }
 export default appConfig

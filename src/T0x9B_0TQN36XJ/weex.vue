49e48c22d9c73de1d6db3a2541afac5745f8718f
sf <530170234@qq.com>
2018-09-29 09:46:40 +0800

    test

---
+++ src/T0x9B_0TQN36XJ/weex.vue
@@ -0,0 +1,76 @@
+<template>
+    <div>
+        {{test}}
+    </div>
+</template>
+
+<style>
+.indicator{
+    bottom: 10px;
+    color: #59a
+}
+</style>
+
+<script>
+    const storage = weex.requireModule('storage')
+    import MideaHeader from '@/midea-component/header.vue'
+    // import mideaItem from '@/midea-component/item.vue'
+    import nativeService from "../common/services/nativeService";
+    import cmdFun from "./util.js"; //解析指令
+    import query from "../dummy/query";
+
+
+    export default {
+        data(){
+            return {
+                test:'123'
+            }
+        },
+        components: {MideaHeader },
+        created(){
+            // nativeService.toast(1);
+            //模拟设备数据
+            // nativeService.initMockData({
+            //     query: query
+            // });
+            this.queryStatus();
+        },
+        methods: {
+            goBack(){
+                nativeService.goBack()
+            },
+            queryStatus() {
+                var self = this;
+                var sendCmd = cmdFun.createQueryMessage();
+                //nativeService.showLoading();
+                nativeService.startCmdProcess(
+                    "query",
+                    sendCmd,
+                    function (result) {
+                        //nativeService.hideLoading();
+                        nativeService.toast(123);
+                        nativeService.toast(result);
+                        var result_arr = result.replace(/\[|]/g, ""); //去掉中括号
+                        var arr = result_arr.split(",");
+                        //nativeService.alert(arr[11]);
+                        var analysisObj = cmdFun.analysisCmd(arr);
+                        // self.analysisFun(analysisObj);
+                        // nativeService.toast(analysisObj);
+                        self.test = JSON.stringify(analysisObj);
+                    },
+                    function (result) {
+                        //nativeService.hideLoading();
+                        nativeService.toast(result);
+                        nativeService.toast("查询失败" + JSON.stringify(result));
+                    }
+                );
+            },
+            goTo(url){
+                let path = url + '.js'
+                nativeService.goTo(path)
+            }
+        }
+    }
+</script>
+
+

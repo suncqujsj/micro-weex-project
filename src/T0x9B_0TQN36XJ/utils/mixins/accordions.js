b231c540ba9acf645970268ca81699ce7d13228b
sf <530170234@qq.com>
2018-10-22 18:15:49 +0800

    preheat默认值

--- src/T0x9B_0TQN36XJ/utils/mixins/accordions.js
+++ src/T0x9B_0TQN36XJ/utils/mixins/accordions.js
@@ -103,7 +103,7 @@ const accordionMixin = {
                 mode: this.currentItem.mode,
                 minute: this.setValue('time'),
                 temperature: this.setValue('temperature'),
-                preheat: this.setValue('preheat'),
+                preheat: this.currentItem.preheat,
                 steamAmount: this.setValue('steamAmount'),
                 fireAmount: this.setValue('fireAmount')
             };

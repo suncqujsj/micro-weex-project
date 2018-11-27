123516405fbdcc6f9fcd42521366ce38522aec6c
unknown <PENGXQ2@cn.midea.com>
2018-11-27 10:01:24 +0800

    更新修改双腔体的目录

--- src/T0x9B_0D50S3AT/config/constant.js
+++ src/T0x9B_0D50S3AT/config/constant.js
@@ -5,8 +5,8 @@
 
 module.exports = {
     device: {
-        type: 0xB2,
-        widget_name: 'xxx_T0x9B_SFTEST',
+        type: 0x9B,
+        widget_name: 'xxx_T0x9B_0D50S3AT',
         widget_version: '1.0.0',
         iot_device_id: '',
     }

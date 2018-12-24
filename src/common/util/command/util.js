343c76b239c8bbbfcdb5d73080a7a6f384989f9d
zy <645303401@qq.com>
2018-12-24 09:49:38 +0800

    add doc title

--- src/common/util/command/util.js
+++ src/common/util/command/util.js
@@ -282,13 +282,9 @@ export default {
   },
 
 
-<<<<<<< HEAD
-    // 炉灯
-    cmdLight(params,device){
-=======
+
    //炉灯
    cmdLight(params,device){
->>>>>>> develop
     var messageBody = message.createMessageBody(7); 
     message.setByte(messageBody, 0,0x22);
     message.setByte(messageBody, 1,0x02);
@@ -302,10 +298,6 @@ export default {
   },
    //上锁
    cmdLock(params,device){
-<<<<<<< HEAD
-
-=======
->>>>>>> develop
     var messageBody = message.createMessageBody(7); 
     message.setByte(messageBody, 0,0x22);
     message.setByte(messageBody, 1,0x02);

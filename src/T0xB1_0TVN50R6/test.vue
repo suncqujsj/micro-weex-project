37079899a130a75343d1edf7486895a0fd832763
sf <530170234@qq.com>
2018-12-28 15:36:00 +0800

    test vue

--- src/T0xB1_0TVN50R6/test.vue
+++ src/T0xB1_0TVN50R6/test.vue
@@ -1,49 +1,34 @@
 <style lang="less" type="text/less">
-    @import "../common/less/common";
 
 </style>
 
 <template>
-    <div class="bg-gray">
-
-        <midea-header class="bg-white" title="测试" titleText="black" :isImmersion="true"  :showLeftImg="true" @leftImgClick="back"></midea-header>
-
-        <midea-switch2 v-if="!hide" style="margin: 100px;" :checked="checked" @change="onchange" slot="value"> </midea-switch2>
-
-        <midea-button text="显示开关" type="secondary" @mideaButtonClicked="mideaButtonClicked">            </midea-button>
-
-
+    <div style="margin-top: 200px;">
+        <text>{{a}}</text>
+        <text>{{b}}</text>
     </div>
 </template>
 
 <script>
-    import mideaHeader from '@/midea-component/header.vue'
-    import mideaCell from '@/midea-component/item.vue';
-    import mideaSwitch2 from '@/midea-component/switch2.vue'
-    import mideaButton from '@/midea-component/button.vue'
 
-    import nativeService from "../common/services/nativeService";
+    import nativeService from '@/common/services/nativeService';
 
     export default {
         data(){
             return {
-                checked:false,
-                hide:true
+                a:123,
+                b:456
             }
         },
-        components: {mideaHeader, mideaCell, mideaSwitch2, mideaButton},
-        created(){},
-        computed:{
+        created(){
+            this.test();
         },
+
         methods: {
-            back: function(){
-                nativeService.goBack();
-            },
-            onchange(event) {
-                // nativeService.alert(event.value);
-            },
-            mideaButtonClicked(){
-                this.hide = false;
+            async test(){
+                this.a = 666;
+                this.b = 777;
+                let c = await nativeService.getDeviceInfo();
             }
         }
     }

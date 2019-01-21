da69ff50c6dc17c52ba17d44fa91c961da158d08
sf <530170234@qq.com>
2019-01-21 16:22:31 +0800

    ota 联调

--- src/T0xB1_0TVN50R6/test.vue
+++ src/T0xB1_0TVN50R6/test.vue
@@ -27,20 +27,9 @@
         methods: {
             async test(){
                 this.a = 666;
-                try {
-                    let c = await d();
-                    nativeService.alert(c);
-                }catch (e) {
-                    nativeService.alert(e);
-                }
-
                 this.b = 777;
-            },
-            d(){
-                return new Promise((resolve, reject) => {
-                    resolve(10);
-                });
-            },
+                // let c = await nativeService.getDeviceInfo();
+            }
         }
     }
 </script>

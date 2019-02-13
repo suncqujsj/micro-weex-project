65495a9b83c10e029e5ce0615aacfc566b3cfecb
sf <530170234@qq.com>
2019-02-13 17:17:02 +0800

    隐藏 last item 下划线

---
+++ src/T0xB0_0M3L20A8/config/more.js
@@ -0,0 +1,19 @@
+/**
+ * Created by sf
+ * 2018/12/27
+ */
+
+import constant from "./constant.js";
+
+let list = [
+    {
+        title: '产品型号：年轻化20升微波炉',
+        link:''
+    },
+    {
+        title: '插件版本号：weex' + constant.device.widget_version,
+        link: ''
+    }
+];
+
+export default list;

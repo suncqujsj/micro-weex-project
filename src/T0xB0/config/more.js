7a1935950833472129f920ef5be1496e84add85f
parker <xingquan.peng@midea.coom>
2019-02-14 10:53:55 +0800

    旧微波炉插件升级

--- src/T0xB0/config/more.js
+++ src/T0xB0/config/more.js
@@ -7,11 +7,13 @@ import constant from "./constant.js";
 
 let list = [
     {
-        title: '产品型号：微波炉',
+        title: '产品型号',
+        rightText: '微波炉X7',
         link:''
     },
     {
-        title: '插件版本号：' + constant.device.widget_version,
+        title: '插件版本号',
+        rightText: constant.device.widget_version,
         link: ''
     }
 ];

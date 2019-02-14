cda96ed333f2f395d7bd2707e21ada675e446878
sf <530170234@qq.com>
2019-02-14 17:09:59 +0800

    修改型号

--- src/T0xB4_08T7428E/config/more.js
+++ src/T0xB4_08T7428E/config/more.js
@@ -7,17 +7,19 @@ import constant from "./constant.js";
 
 let list = [
     {
-        title: '产品型号：辅助蒸汽烤箱 ',
+        title: '产品型号 ',
+        rightText: '美的Fun烤箱II代',
         link:''
     },
     {
-        title: '插件版本号：weex' + constant.device.widget_version,
+        title: '插件版本号',
+        rightText: constant.device.widget_version,
         link: ''
     },
-    {
-        title: '烤箱实时监控',
-        link:'video.js'
-    }
+    // {
+    //     title: '烤箱实时监控',
+    //     link:'video.js'
+    // }
 ];
 
 export default list;

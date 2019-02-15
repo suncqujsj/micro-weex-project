183dba7e5d5f5f52f64a28f2e770ea6d8c5205b6
sf <530170234@qq.com>
2019-02-15 16:57:55 +0800

    修改文案

--- src/T0xB4_08T7428E/config/constant.js
+++ src/T0xB4_08T7428E/config/constant.js
@@ -5,7 +5,7 @@
 
 module.exports = {
     device: {
-        page_title: "美的Fun烤箱II代",
+        page_title: "烤箱",
         type: 0xB4,
         widget_name: 'MSO_T0xB1',  // 埋点的时候，只保留 B0，B1，B2
         widget_version: '5.0.0',

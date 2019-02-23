3d9a6a0f229646707a917e97ec3006a67c83b1ce
sf <530170234@qq.com>
2019-02-23 16:22:20 +0800

    配置

--- src/T0xBF_0TR934MJ/config/modes.js
+++ src/T0xBF_0TR934MJ/config/modes.js
@@ -9,37 +9,113 @@ let modes = [
         title: '',
         iconButtons: [
             {
+                // 蒸汽
                 'icon': modeIcons[33].dir,
                 'text': modeIcons[33].cn,
                 'mode': 0x20,
                 time: {
                     set: true,
-                    default:5,
-                    range:[1,95,1],
+                    default:10,
+                    range:[1,60,1, 65,90,5]
                 },
                 temperature:{
                     set: false,
-                    default:null,
-                    range:null,
+                    default:100,
+                    range:[50,100,10],
                 },
-                preheat:{
-                    set:false,
-                    default: null,
-                    hide: true, //工作中，隐藏预热选择
+                settingHide:true, // 工作页面，收否隐藏设置参数，true = 隐藏，false = 显示
+
+            },
+            {
+                // 热风对流
+                'icon': modeIcons[1].dir,
+                'text': modeIcons[1].cn,
+                'mode': 0x41,
+                time: {
+                    set: true,
+                    default:10,
+                    range:[1,60,1, 65,90,5]
                 },
-                steamAmount:{
-                    set:false,
-                    default:null,
-                    range:null,
+                temperature:{
+                    set: true,
+                    default:180,
+                    range:[50,50,1,100,230,5],
+                },
+                settingHide:true, // 工作页面，收否隐藏设置参数，true = 隐藏，false = 显示
+
+            },
+            {
+                // 热风对流
+                'icon': modeIcons[31].dir,
+                'text': modeIcons[31].cn,
+                'mode': 0x43,
+                time: {
+                    set: true,
+                    default:10,
+                    range:[1,60,1, 65,120,5, 150,300,30]
+                },
+                temperature:{
+                    set: true,
+                    default:180,
+                    range:[35,35,1,100,180,5],
+                },
+                settingHide:true, // 工作页面，收否隐藏设置参数，true = 隐藏，false = 显示
+
+            },
+            {
+                // 上管
+                'icon': modeIcons[15].dir,
+                'text': modeIcons[15].cn,
+                'mode': 0x40,
+                time: {
+                    set: true,
+                    default:10,
+                    range:[1,60,1, 65,120,5, 150,300,30]
+                },
+                temperature:{
+                    set: false,
+                    default:180,
+                    range:[35,35,1,100,180,5],
                 },
                 fireAmount:{
                     set:true,
-                    default:30,
-                    range:[30,50,20,80,80,1,100,100,1],
-                    hide: true
+                    default:3,
+                    range:[1,3,1]
                 },
-               
+                settingHide:true, // 工作页面，收否隐藏设置参数，true = 隐藏，false = 显示
+
+            }
+        ]
+    },
+    {
+        title: '',
+        iconButtons: [
+            {
+                // 微波
+                'icon': modeIcons[12].dir,
+                'text': modeIcons[12].cn,
+                'mode': 0x01,
+                time: {
+                    set: true,
+                    default:1,
+                    range:[1,30,1]
+                },
+                settingHide:true, // 工作页面，收否隐藏设置参数，true = 隐藏，false = 显示
+
             },
+            {
+                // 清洁
+                'icon': modeIcons[20].dir,
+                'text': modeIcons[20].cn,
+                'mode': 0xC6,
+                time: {
+                    set: false,
+                    default:20,
+                    range:[20,20,1]
+                },
+                settingHide:true, // 工作页面，收否隐藏设置参数，true = 隐藏，false = 显示
+
+            }
         ]
     },
 ];

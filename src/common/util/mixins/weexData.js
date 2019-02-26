94a415d9f59dd474f2be60b598077f359dcf85ca
sf <530170234@qq.com>
2019-02-26 15:45:24 +0800

    可编辑判断

--- src/common/util/mixins/weexData.js
+++ src/common/util/mixins/weexData.js
@@ -249,9 +249,7 @@ let workingModalMixin  = {
             }
             var _item = cmdFun.getCurrentModeItem(tabs,analysisObj.recipeId.value,analysisObj.mode.value,_isRecipe);
             //this.currentItem = _item;
-            if(_item.settingHide){
-                this.hasSetting = false;
-            }
+            this.settingHide(_item);
             if(_item.circleProgressPointHide){
                 chartJson.pointShow = false;
             }
@@ -295,9 +293,7 @@ let workingModalMixin  = {
                 this.statusTag = '';
                 this.hasSetting = true;
                 this.hasStopOrContinueBtn = true;
-                if(_item.settingHide){
-                    this.hasSetting = false;
-                }
+                this.settingHide(_item);
                 chartJson.pointShow = false;
                 
             }
@@ -367,6 +363,15 @@ let workingModalMixin  = {
                 }
             }
 
+        },
+        /**
+         * 工作状态中 模式不可编辑设置
+         */
+        settingHide(item){
+            nativeService.alert(item);
+            if(item.settingHide || item.standbyHide) {
+                this.hasSetting = false;
+            }
         },
         cancle(){
             var self = this;

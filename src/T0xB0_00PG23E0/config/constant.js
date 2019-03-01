17fe6ed831114be915f4077e3dde5ca731807fb2
zy <645303401@qq.com>
2019-03-01 13:42:39 +0800

    新增加一款微波炉（g100改名复制而来），与现有g100所有功能一样，仅仅改文案配置

---
+++ src/T0xB0_00PG23E0/config/constant.js
@@ -0,0 +1,21 @@
+/**
+ * Created by sf
+ * 2018/10/26
+ */
+
+module.exports = {
+    device: {
+        type: 0xB0,
+        page_title: "微波炉",
+        widget_name: 'MSO_T0xB0',
+        widget_version: '5.0.6',
+        extra1: {
+            sn8: '0X5L23A8'
+        },
+        standby03:true, // 待机中打开03轮询
+        // hms:[0,1,1],
+        hideChildLock:{
+            working:true
+        }
+    }
+};

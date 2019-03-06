afb42d8a7a56eeb26f735b6c3ca2db61fc51e47d
sf <530170234@qq.com>
2019-03-06 17:14:00 +0800

    v项目文件

---
+++ src/T0xB0_xxxxxxxx/config/more.js
@@ -0,0 +1,21 @@
+/**
+ * Created by sf
+ * 2018/12/27
+ */
+
+import constant from "./constant.js";
+
+let list = [
+    {
+        title: '产品型号',
+        rightText: 'M3-L20A8',
+        link:''
+    },
+    {
+        title: '插件版本号',
+        rightText: constant.device.widget_version,
+        link: ''
+    }
+];
+
+export default list;

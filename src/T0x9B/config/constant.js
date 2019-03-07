66a703308787c6837fb57ab68bc5dfecf852bf5f
zy <645303401@qq.com>
2019-03-07 17:29:39 +0800

    修改 夏工提交bug v2

--- src/T0x9B/config/constant.js
+++ src/T0x9B/config/constant.js
@@ -9,7 +9,7 @@ module.exports = {
         // page_title: "蒸汽烤箱", // 此字段已作废
         //sales_model: "TPN50QL_SSL", // 此字段已作废
         widget_name: 'MSO_T0xB1',
-        widget_version: '5.0.4',
+        widget_version: '5.0.6',
         extra1: {
             sn8: '0TQN50QL'
         },
@@ -20,3 +20,11 @@ module.exports = {
         // hms:[0,1,1] //是否用时分秒组件
     }
 };
+
+
+/*
+* 注意事项：
+* 1。由于老版本插件工作中没有上报 设定时间，因此 剩余时间/设定时间 的进度条将无法实现，因此隐藏进度小圆点 circleProgressPointHide: true,
+* 2。老版本插件工作中不可以改时间和温度，避免出现问题。
+*
+* */

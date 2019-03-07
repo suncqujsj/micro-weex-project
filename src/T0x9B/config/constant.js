2d03a2f43d311f60149915d98a1fe782dc6513f6
zy <645303401@qq.com>
2019-03-07 13:41:04 +0800

    修改 constant

--- src/T0x9B/config/constant.js
+++ src/T0x9B/config/constant.js
@@ -7,14 +7,14 @@ module.exports = {
     device: {
         type: 0x9B,
         page_title: "蒸汽烤箱",
-        sales_model: "TPN36FQL_SSL",
-        widget_name: 'MSO_T0xB2',
-        widget_version: '5.0.2',
+        sales_model: "TPN50QL_SSL",
+        widget_name: 'MSO_T0xB1',
+        widget_version: '5.0.3',
         extra1: {
-            sn8: '0TPN36FQ'
+            sn8: '0TQN50QL'
         },
         hideChildLock:{
-            standby:true,
+            standby:false,
             working:true
         }
         // hms:[0,1,1] //是否用时分秒组件

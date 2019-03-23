a47165a9361c80053b78ad52e10f4b8a0b1ac237
sf <530170234@qq.com>
2019-03-23 11:12:43 +0800

    修改版本号

--- src/T0xB4_0T7L421F/config/constant.js
+++ src/T0xB4_0T7L421F/config/constant.js
@@ -8,7 +8,7 @@ module.exports = {
         page_title: "烤箱",
         type: 0xB4,
         widget_name: 'MSO_T0xB1',  // 埋点的时候，只保留 B0，B1，B2
-        widget_version: '5.0.3',
+        widget_version: '5.0.5',
         extra1: {
             sn8: '0T7L421F'
         },

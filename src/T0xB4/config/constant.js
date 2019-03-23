4ee88d30edcc45101da43e959019dc09f93f9ae6
parker <xingquan.peng@midea.coom>
2019-03-23 14:10:51 +0800

    fixed 旧协议弹出框闪烁问题 bug

--- src/T0xB4/config/constant.js
+++ src/T0xB4/config/constant.js
@@ -9,7 +9,7 @@ module.exports = {
         type: 0xB4,
         sales_model: "T7-388D",
         widget_name: 'MSO_T0xB1',  // 埋点的时候，只保留 B0，B1，B2
-        widget_version: '5.1.3',
+        widget_version: '5.1.6',
         extra1: {
             sn8: '00T7388D'
         },

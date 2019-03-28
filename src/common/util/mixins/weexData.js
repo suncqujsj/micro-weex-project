7b3f21eb9e0b41ae725409579b1325e9becbc0bd
sf <530170234@qq.com>
2019-03-28 16:43:50 +0800

    温度 单位 复用

--- src/common/util/mixins/weexData.js
+++ src/common/util/mixins/weexData.js
@@ -240,14 +240,14 @@ let workingModalMixin  = {
             }
 
             if(this.isLargeOven1065()){ //大烤箱旧插件 0ET1065Q ，上报的温度问题，下管上报了错乱的温度...需要只读上管温度
-                customData.temperatureText = cmdObj.temperature.upLowTemperature +'°'; 
+                customData.temperatureText = this.addTemperatureUnit(cmdObj.temperature.upLowTemperature);
             }
 
             if(this.isSteamOven36L() && cmdObj.isProbe.value==1){//如果是旧插件36L蒸汽烤箱0TQN36QL，探针模式下，强行转换为非探针模式，因为该型号，探针模式下，还可以启动所有的模式
                 if(this.currentIsWorkingPage(cmdObj)){
                     if(cmdObj.mode.value!=0x31 && cmdObj.mode.value!=0x33 && cmdObj.mode.value!=0x3A){
                         cmdObj.isProbe.value = 0;
-                        customData.temperatureText = cmdObj.temperature.upLowTemperature +'°'; 
+                        customData.temperatureText = this.addTemperatureUnit(cmdObj.temperature.upLowTemperature);
                     }
                 }
             }

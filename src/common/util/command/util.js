dcca5240d888711b6611fc18538180fe07a6f17a
sf <530170234@qq.com>
2019-03-29 15:33:56 +0800

    温度设置

--- src/common/util/command/util.js
+++ src/common/util/command/util.js
@@ -378,7 +378,7 @@ export default {
         }
 
         var sendcmd = message.createMessage(callbackData.device.type, 0x02, messageBody);
-        nativeService.alert(this.cmdToEasy(sendcmd));
+        // nativeService.alert(this.cmdToEasy(sendcmd));
         return sendcmd;
     },
 

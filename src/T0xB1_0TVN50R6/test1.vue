b39780f36c34bafd98a4af2ff32422ea7c24c261
sf <530170234@qq.com>
2019-05-18 18:25:56 +0800

    test code

--- src/T0xB1_0TVN50R6/test1.vue
+++ src/T0xB1_0TVN50R6/test1.vue
@@ -11,6 +11,36 @@
     import nativeService from '@/common/services/nativeService';
     import timePicker from '@/midea-component/sf/custom/timePicker.vue'
 
+    Array.prototype.multiply = function(){
+        this.map(()=>{
+            this.push(item*item);
+        },this)
+    };
+
+    // Array.prototype.multiply = function(){
+    //     this.push(...this.map(i => i*i));
+    // };
+
+    const actions = new Map([
+        [1, ['processing','IndexPage']],
+        [2, ['fail','FailPage']],
+        [3, ['fail','FailPage']],
+        [4, ['success','SuccessPage']],
+        [5, ['cancel','CancelPage']],
+        ['default', ['other','Index']]
+    ]);
+
+    const users = [{
+        name: 'Yazeed',
+        age: 25
+    }, {
+        name: 'Sam',
+        age: 30
+    }, {
+        name: 'Bill',
+        age: 20
+    }];
+
     export default {
         data(){
             return {
@@ -19,12 +49,28 @@
             }
         },
         created(){
+            // const a = [1,2,3,4,5];
+            // a.multiply()
+            // nativeService.alert(a);
+            nativeService.alert(users.map(this.getName))
+            // this.test()
+
         },
         components:{timePicker},
         methods: {
             onChange(e){
                 // nativeService.alert(e.value);
-            }
+            },
+            test : (status)=>{
+                let action = actions.get(status) || actions.get('default')
+                nativeService.alert(action)
+            },
+            sayHi : (name) => `Hi, ${name}!`,
+            double: (x) => x*2,
+            getFirstName: (obj) => obj.firstName,
+            len: (array) => array.length,
+            even: (num) => num%2 === 0,
+            getName: (user) => user.name
         }
     }
 </script>

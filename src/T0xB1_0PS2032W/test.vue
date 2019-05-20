2d2a4a266f9f67462ed64a1215b9d571bdc06164
zy <645303401@qq.com>
2019-05-20 14:53:17 +0800

    新增 s20 蒸烤箱 插件， 初步完成 mode 和 食谱配置

---
+++ src/T0xB1_0PS2032W/test.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="wrapper">
+        <!--<div ref="tests" @click="move" class="box"></div>-->
+        <!--<image ref="tests" src="img/loading.png" class="box"></image>-->
+        <midea-button text="打开" type="primary" @mideaButtonClicked="mideaButtonClicked1">
+        </midea-button>
+        <sf-state :display="display" :isMask="true" :text="'更新完成'" :type="'success'"></sf-state>
+    </div>
+</template>
+
+<script>
+    const animation = weex.requireModule('animation')
+    const modal = weex.requireModule('modal')
+    import nativeService from "@/common/services/nativeService";
+    import sfState from "@/midea-component/sf/custom/state.vue"
+    import mideaButton from '@/midea-component/button.vue'
+
+
+    export default {
+        data(){
+            return{
+               display:false,
+            };
+        },
+        created(){
+            // var context = this;
+            // var t = setInterval(function(){
+            //   context.move()
+            // },20)
+        },
+        components:{sfState, mideaButton},
+        methods: {
+            mideaButtonClicked1(){
+                this.display = true;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .box {
+        width: 64px;
+        height: 64px;
+        margin-left: 100px;
+        margin-top: 100px;
+    }
+</style>

a211ccdb464aa2cd08cefce58e84ddd4605b9f93
MAXIAO2 <xiao2.ma@midea.com>
2019-06-13 20:16:09 +0800

    S20项目修改

--- src/T0xB1_0PS2032W/weex.vue
+++ src/T0xB1_0PS2032W/weex.vue
@@ -6,6 +6,7 @@
     // config data
     import constant from "./config/constant";
     import modes from "./config/modes.js";
+    import autoMenu from "./config/auto-menu.js";
     import commonWeex from "@/common/pages/weex.vue";
     import nativeService from "@/common/services/nativeService";
 
@@ -14,6 +15,11 @@
         data(){
             return {
                 tabs:[
+                    {
+                        name:'自动菜单',
+                        active:false,
+                        rows:autoMenu
+                    },
                     {
                         name:'加热模式',
                         active:true,
@@ -25,9 +31,9 @@
         },
         components:{commonWeex},
         created(){
-            let context = this;
-            nativeService.setItem("modeData",context.tabs);
-            nativeService.setItem("constantData",context.constant);
+            // let context = this;
+            // nativeService.setItem("modeData",context.tabs);
+            // nativeService.setItem("constantData",context.constant);
         },
         methods: {
            

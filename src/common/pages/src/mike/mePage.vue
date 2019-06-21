ebe876428e6e19b44eaa293226245944e29bb17d
彭兴权 <15915832580@163.com>
2019-06-21 13:51:45 +0800

    开发mePage 页面

--- src/common/pages/src/mike/mePage.vue
+++ src/common/pages/src/mike/mePage.vue
@@ -1,9 +1,61 @@
 <style lang="less" type="text/less">
+.hearder-section{
+  height: 500px;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #333;
+}
+.header-icon{
+  border-radius: 100px;
 
+}
+.header-name{
+  color: white;
+  font-size: 40px;
+  margin-top: 20px;
+}
+.header-sub-name{
+  color: #dddddd;
+  margin-top: 10px;
+}
+
+.select-tags{
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  margin-top: 30px;
+}
+.tag-item{
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 200px;
+}
+.tag-item-icon{
+
+}
+.tag-item-number{
+  color:#dddddd;
+}
 </style>
 <template>
     <div>
-      <text style="color: black"> me home </text>
+       <div class="hearder-section">
+          <image class="header-icon" src="https://mapnew.midea.com/pfile/group1/M00/6A/49/ChAgb1le8o6AJxDcAAAaX6S8hL8860.jpg" style="width:200px;height:200px"></image>
+          <text class="header-name">parker</text>
+          <text class="header-sub-name">ID：parker</text>
+          <div class="select-tags">
+              <div class="tag-item" v-for="(item,index) in selectTags">
+                  <image class="tag-item-icon" :src="item.icon" style="width:50px;height:50px"></image>
+                  <text class="tag-item-number">{{item.number}}</text>
+              </div>
+          </div>
+       </div>
+       <div class="">
+          
+          <image src="http://121.41.75.163:8000/source/image/20181126/1543210828723ngy8.jpg" style="width:200px;height:100px"></image>
+       </div>
     </div>
 </template>
 
@@ -12,6 +64,11 @@
     props: [],
     data(){
         return {
+          selectTags: [
+            {icon: "img/header/public_ic_home@3x.png",number:111},
+            {icon: "img/header/public_ic_home@3x.png",number:111},
+            {icon: "img/header/public_ic_home@3x.png",number:111},
+          ]
         }
     },
     methods:{
--- src/common/pages/src/mike/recipePage.vue
+++ src/common/pages/src/mike/recipePage.vue
@@ -1,98 +1,95 @@
 <style lang="less" type="text/less">
-    .wrapper {
-        width: 750px;
-        display: flex;
-        flex-direction: column;
-    }
+.wrapper {
+  width: 750px;
+  display: flex;
+  flex-direction: column;
+}
 
-    .list {
+.list {
+}
 
-    }
+.search-tag {
+  height: 200px;
+  width: 750px;
+}
 
-    .search-tag {
-        height: 200px;
-        width: 750px;
-    }
+.panel {
+  width: 600px;
+  height: 300px;
+  margin-left: 75px;
+  margin-top: 35px;
+  margin-bottom: 35px;
+  flex-direction: column;
+  justify-content: center;
+  border-width: 2px;
+  border-style: solid;
+  border-color: rgb(162, 217, 192);
+  background-color: rgba(162, 217, 192, 0.2);
+}
 
+.text {
+  font-size: 88px;
+  text-align: center;
+  color: #41b883;
+}
 
-    .panel {
-        width: 600px;
-        height: 300px;
-        margin-left: 75px;
-        margin-top: 35px;
-        margin-bottom: 35px;
-        flex-direction: column;
-        justify-content: center;
-        border-width: 2px;
-        border-style: solid;
-        border-color: rgb(162, 217, 192);
-        background-color: rgba(162, 217, 192, 0.2);
-    }
+.swiper {
+  flex-direction: column;
+}
 
-    .text {
-        font-size: 88px;
-        text-align: center;
-        color: #41B883;
-    }
+.swiper-slider {
+  width: 750px;
+  height: 900px;
 
-    .swiper {
-        flex-direction: column;
-    }
+  position: relative;
+}
 
-    .swiper-slider {
-        width: 750px;
-        height: 900px;
+.slider-img {
+  width: 750px;
+  height: 900px;
+}
 
-        position: relative;
-    }
+.slider-indicator {
+  width: 750px;
+  height: 100px;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  item-color: rgba(255, 255, 255, 0.5);
+  item-selectedcolor: rgba(255, 255, 255, 1);
+  item-size: 12px;
+}
 
-    .slider-img {
-        width: 750px;
-        height: 900px;
-    }
+.search-tag {
+  height: 200px;
+  width: 750px;
+}
 
-    .slider-indicator {
-        width: 750px;
-        height: 100px;
-        position: absolute;
-        bottom: 0;
-        left: 0;
-        item-color: rgba(255, 255, 255, .5);
-        item-selectedColor: rgba(255, 255, 255, 1);
-        item-size: 12px;
-    }
+.recipe-section {
+  width: 650px;
+  height: 300px;
+  margin-left: 50px;
+  margin-top: 35px;
+  margin-bottom: 35px;
+  flex-direction: column;
+  justify-content: center;
+  // border-width: 2px;
+  // border-style: solid;
+  // border-color: rgb(162, 217, 192);
+  background-color: rgba(162, 217, 192, 0.2);
+}
 
-    .search-tag {
-        height: 200px;
-        width: 750px;
-    }
+.text {
+  font-size: 50px;
+  text-align: center;
+  color: #333;
+}
 
-
-    .recipe-section {
-        width: 650px;
-        height: 300px;
-        margin-left: 50px;
-        margin-top: 35px;
-        margin-bottom: 35px;
-        flex-direction: column;
-        justify-content: center;
-        // border-width: 2px;
-        // border-style: solid;
-        // border-color: rgb(162, 217, 192);
-        background-color: rgba(162, 217, 192, 0.2);
-    }
-
-    .text {
-        font-size: 50px;
-        text-align: center;
-        color: #333;
-    }
-
-    .recipe-section-title {
-        font-size: 40px;
-        text-align: left;
-        color: #333;
-    }
+.recipe-section-title {
+  font-size: 40px;
+  text-align: left;
+  color: #333;
+}
 </style>
 <template>
 
@@ -140,75 +137,87 @@
 </template>
 
 <script>
-    import nativeService from "@/common/services/nativeService";
+import nativeService from "@/common/services/nativeService";
 
-    const stream = weex.requireModule('stream');
-    import RecipeSearchBar from "@/common/pages/src/mike/component/recipeSearchBar.vue";
-    import RecipeTags from "@/common/pages/src/mike/component/recipeTags.vue";
+const stream = weex.requireModule("stream");
+import RecipeSearchBar from "@/common/pages/src/mike/component/recipeSearchBar.vue";
+import RecipeTags from "@/common/pages/src/mike/component/recipeTags.vue";
 
-    export default {
-        components: {RecipeTags, RecipeSearchBar},
-        props: [],
-        data() {
-            return {
-                tags: [{
-                    text: '快手菜',
-                    selected: true,
-                }, {
-                    text: '轻食塑身',
-                    selected: false,
-                }, {
-                    text: '烘焙/甜点',
-                    selected: false,
-                }, {
-                    text: '西式烹饪',
-                    selected: false,
-                }, {
-                    text: '健康饮食',
-                    selected: false,
-                }, {
-                    text: '川香麻辣',
-                    selected: false,
-                }],
-                covers: [],
-                collectionData: [],
-            }
+export default {
+  components: { RecipeTags, RecipeSearchBar },
+  props: [],
+  data() {
+    return {
+      tags: [
+        {
+          text: "快手菜",
+          selected: true
         },
-        created() {
-            let self = this;
-            //测试接口
-            let _url = "http://120.25.95.38:8200/cloud-menu/home/midea/menu/collection/all";
-            let _body = JSON.stringify({});
-            let requestData = {url: _url, body: _body};
-            nativeService.sendHttpRequest(requestData).then(function (res) {
-                self.collectionData = res.data;
-                nativeService.alert(res);
-            }).catch((resp) => {
-                nativeService.alert(resp);
-            })
-
-            this.loadCovers();
+        {
+          text: "轻食塑身",
+          selected: false
         },
-        methods: {
-            loadCovers() {
-                stream.fetch({
-                    url: 'https://picsum.photos/v2/list?limit=5&page=1',
-                    method: 'GET',
-                    type: 'json'
-                }, response => {
-
-                    this.covers = response.data;
-
-                })
-            },
-            loadmore() {
-                let self = this;
-                // setTimeout(() => {
-                //   for(var i=0; i< 10; i++){
-                //     self.lists.push(i+'dd');
-                //   }
-                // }, 100);
-            }
+        {
+          text: "烘焙/甜点",
+          selected: false
+        },
+        {
+          text: "西式烹饪",
+          selected: false
+        },
+        {
+          text: "健康饮食",
+          selected: false
+        },
+        {
+          text: "川香麻辣",
+          selected: false
         }
+      ],
+      covers: [],
+      collectionData: []
+    };
+  },
+  created() {
+    let self = this;
+    //测试接口
+    let _url =
+      "http://120.25.95.38:8200/cloud-menu/home/midea/menu/collection/all";
+    let _body = JSON.stringify({});
+    let requestData = { url: _url, body: _body };
+    nativeService
+      .sendHttpRequest(requestData)
+      .then(function(res) {
+        self.collectionData = res.data;
+        // nativeService.alert(res);
+      })
+      .catch(resp => {
+        nativeService.alert(resp);
+      });
+
+    this.loadCovers();
+  },
+  methods: {
+    loadCovers() {
+      stream.fetch(
+        {
+          url: "https://picsum.photos/v2/list?limit=5&page=1",
+          method: "GET",
+          type: "json"
+        },
+        response => {
+          this.covers = response.data;
+        }
+      );
+    },
+    loadmore() {
+      let self = this;
+      // setTimeout(() => {
+      //   for(var i=0; i< 10; i++){
+      //     self.lists.push(i+'dd');
+      //   }
+      // }, 100);
     }
+  }
+};
 </script>
